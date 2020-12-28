4321f1e8933ad3899df1a5be4e5fdc0dca911a53
snx <snx1029@163.com>
2020-12-28 16:15:51 +0800

    prepare

---
+++ tree/BinaryTree.java
@@ -0,0 +1,19 @@
+public class BinaryTree{
+	public class TreeNode{
+		int data;
+		TreeNode left;
+		TreeNode right;
+		public TreeNode(data){
+			this.data = data;
+		}
+	}
+	TreeNode root;
+	
+	public void traversal(TreeNode root){
+
+	}
+
+	public static void main(String[] args) {
+		
+	}
+}
